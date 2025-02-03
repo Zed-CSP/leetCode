f50f82972d95edb847de2b1f9150c5d9964e2933
Christopher Peret <cperet89@gmail.com>
2025-02-02 18:40:09 -0800

    revised carry for edge case 2/2/25

--- questions/02_Add_Two_Numbers/code.py
+++ questions/02_Add_Two_Numbers/code.py
@@ -9,7 +9,7 @@ class Solution(object):
         carry = 0
     
         # Traverse both lists until both are None
-        while l1 or l2:
+        while l1 or l2 or carry:
             # Get the current values, if the node is None, use 0
             x = l1.val if l1 else 0
             y = l2.val if l2 else 0
